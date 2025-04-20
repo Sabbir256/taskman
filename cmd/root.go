e2c27c451e1b28523d2a65114bba158f2e460234
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2025-04-20 17:48:53 +0600

    feat: update the usage command for the project

--- cmd/root.go
+++ cmd/root.go
@@ -8,12 +8,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "tasks",
-	Short: "CLI app for managing todos",
-	Long: `A simple CLI application to manage todos.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "taskman",
+	Short: "A simple and powerful CLI task manager",
+	Long: `Manage your tasks from command line with ease.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -26,14 +23,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tasks.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
